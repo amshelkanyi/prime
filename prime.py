a83d7df18508d5a50d66d8878c66df6e00701e46
Amshel Kanyi <amshel20@gmail.com>
2017-06-13 22:19:45 +0300

    test if input is less than zero or not an int

--- prime.py
+++ prime.py
@@ -1,10 +1,16 @@
 def list_primes(N):
-    primes=[]
-    for num in range(1, (N+1)):
-       if N > 1:
-           for i in range(2, num):
-               if num % i == 0:
-                   break
-           else:
-               primes.append(num)
-    return primes
+    if not isinstance(N, int):
+        return 'Only Integers are allowed'
+    elif N<1:
+        return 'Positive numbers only'
+    else:
+        primes=[]
+        for num in range(2, (N+1)):
+           if N > 1:
+               for i in range(2, num):
+                   if num % i == 0:
+                       break
+               else:
+                   primes.append(num)
+        return primes
+print(list_primes(10))
